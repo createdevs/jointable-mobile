d2b6f7dd4cd5a781265946fa27a1ff932939f91f
butsoleg <olegbuts08@gmail.com>
2020-01-25 23:06:45 -0600

    adjusted authenication.js for auth0

--- server/src/authentication.js
+++ server/src/authentication.js
@@ -1,13 +1,28 @@
 const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
 const { LocalStrategy } = require('@feathersjs/authentication-local');
-const { expressOauth } = require('@feathersjs/authentication-oauth');
+const { expressOauth, OAuthStrategy } = require('@feathersjs/authentication-oauth');
+
+class Auth0Strategy extends OAuthStrategy {
+  async getEntityData(profile) {
+    const baseData = await super.getEntityData(profile);
+
+    return {
+      ...baseData,
+      email: profile.email
+    };
+  }
+}
 
 module.exports = app => {
   const authentication = new AuthenticationService(app);
 
   authentication.register('jwt', new JWTStrategy());
+  authentication.register('local', new LocalStrategy());
+  authentication.register('auth0', new Auth0Strategy());
   
-
   app.use('/authentication', authentication);
   app.configure(expressOauth());
 };
+
+// Additionally, auth0Id needs to be included in the data in the users service class.
+
