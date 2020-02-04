2e238949f90104bf386e6b37abf6736c6e356387
butsoleg <olegbuts08@gmail.com>
2020-02-03 21:28:52 -0600

    adding readme

--- README.md
+++ README.md
@@ -37,26 +37,21 @@ pwd = YOURPASSWORD
 ```
 Check that you can connect to MySQL and your database.
 
-The client side 'app' also needs a .env file with the following variables:
+The client side 'app' also needs a .env file with the following variable, indicating the location of your running server. 
 ```
-andriodId='1234.apps.googleusercontent.com';
-iphoneId='1234.apps.googleusercontent.com';
-deployment=localhost
+deployment=LOCALHOST or YOURSERVERSIPADDRESS
 ```
-The androidId and iphoneId are the google client IDs for the google authentication and will need to be acquired from Google's developer console by making a new project and creating new OAuth client IDs.
-
-The deployment variable is the location of your running server, whether that's localhost (see note at the bottom), tunneling, or have a deployed server with an IP address.
 
 
 ## Source Files
 ### assets 
-  * 3 folders in here: fonts, icons and images
+  * 2 folders in here: fonts and images
 
 ### components 
   * This is where we place all our shared React components. Usually these components are the ones that we call stateless or function components, that have no state logic and can be easily reused across the app.
 
 ### screens
-  * These are our application screens, the ones that we navigate from one to another. 
+  * These are our application screens that we navigate from one to another. 
 
 ## Built With
 
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -32,39 +32,13 @@ export default function HomeScreen() {
         <View style={styles.getStartedContainer}>
           <DevelopmentModeNotice />
 
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
+          <Text style={styles.getStartedText}>JOIN THE TABLE</Text>
 
           <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
+            We're going to schedule some game nights real soon
           </Text>
         </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
       </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
     </View>
   );
 }
